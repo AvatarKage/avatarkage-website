f4a0fa9cc442ae14db74319120fa9fb05f625f25
AvatarKage <contact@avatarkage.com>
2023-12-21 14:04:52 -0500

    adjustments

--- scripts/snowfall.js
+++ scripts/snowfall.js
@@ -14,35 +14,55 @@ document.addEventListener("DOMContentLoaded", function () {
     ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
 
     const flakes = [];
+    const useCustomImage = false; // Set this to true to use a custom image
+    const customImage = new Image();
+    
+    if (useCustomImage) {
+        customImage.src = "media/images/avatarkage/branding/avatarkage_logo_t_512_png.png"; // Replace with your image path
+    }
 
     function createSnowflake() {
-        return {
-        x: Math.random() * canvas.width,
-        y: -5,
-        size: Math.random() * 4 + 1,
-        speed: Math.random() * 2 + 1,
-        opacity: Math.random(),
-        };
+        if (useCustomImage && customImage.src != "") {
+            return {
+                x: Math.random() * canvas.width,
+                y: -5,
+                size: Math.random() * 50 + 1,
+                speed: Math.random() * 2 + 1,
+                opacity: Math.random(),
+                image: customImage,
+            };
+        } else {
+            return {
+                x: Math.random() * canvas.width,
+                y: -5,
+                size: Math.random() * 4 + 1,
+                speed: Math.random() * 2 + 1,
+                opacity: Math.random(),
+            };
+        }
     }
 
     function drawSnowflake(snowflake) {
-        ctx.fillStyle = `rgba(255, 255, 255, ${snowflake.opacity})`;
-        ctx.beginPath();
-        ctx.arc(snowflake.x, snowflake.y, snowflake.size, 0, Math.PI * 2);
-        ctx.fill();
+        if (useCustomImage && customImage.src != "") {
+            ctx.drawImage(snowflake.image, snowflake.x, snowflake.y, snowflake.size, snowflake.size);
+        } else {
+            ctx.fillStyle = `rgba(255, 255, 255, ${snowflake.opacity})`;
+            ctx.beginPath();
+            ctx.arc(snowflake.x, snowflake.y, snowflake.size, 0, Math.PI * 2);
+            ctx.fill();
+        }
     }
 
     function updateSnowflakes() {
         for (let i = flakes.length - 1; i >= 0; i--) {
-        const snowflake = flakes[i];
-
-        snowflake.y += snowflake.speed;
-
-        if (snowflake.y > canvas.height) {
-            // Wrap the snowflake to the top if it reaches the bottom
-            snowflake.y = -5;
-            snowflake.x = Math.random() * canvas.width;
-        }
+            const snowflake = flakes[i];
+    
+            snowflake.y += snowflake.speed;
+    
+            if (snowflake.y > canvas.height) {
+                // Remove the snowflake if it goes beyond the canvas height
+                flakes.splice(i, 1);
+            }
         }
     }
 
@@ -50,7 +70,7 @@ document.addEventListener("DOMContentLoaded", function () {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         flakes.forEach((snowflake) => {
-        drawSnowflake(snowflake);
+            drawSnowflake(snowflake);
         });
     }
 
@@ -77,8 +97,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     animate();
 
-    // Create a new snowflake every second
+    // Create a new snowflake every 200 milliseconds
     setInterval(() => {
         flakes.push(createSnowflake());
-    }, 100);
+    }, 200);
 });
